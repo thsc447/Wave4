6755859e3703a52dd7a796ada8ff941762b99382
thsc447 <thiago.campos@mercadolivre.com>
2022-01-11 12:49:53 -0300

    sqlII ex01

---
+++ SQL/SQLII.sql
@@ -0,0 +1,20 @@
+/* 1 - Join é uma função de agregação que recebe dois ou mais paremetros e retorn a junção dessas tabelas */
+
+/* 2 - Inner Join - Recebe dois parametros e retorna os resultados que estão em ambos as tabelas */
+/* 2 - Left Join - Recebe dois parametros e retorna os resultados que estão em ambos as tabela passada a direita e todos os resultados da tabela a esquerda */
+
+/* 3 - Group by é utilizado para informar ao SQL qual coluna ele utilizara para agrupar uma consulta */
+
+/* 4 - Having é utilizado para filtrar uma agragação. Como se fosse um if */
+
+/* 5 - Inner Join e Right Join */
+
+/* 6 - SELECT * FROM A RIGHT INNER JOIN B | SELECT * FROM A FULL OUTER JOIN B/
+
+/* -------------- */
+/* EX02 */
+/* -------------- */
+
+
+
+
